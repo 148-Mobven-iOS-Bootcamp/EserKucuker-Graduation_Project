d3a43ffa3ecc1af0d376b76c7d1f765fb13f18e7
Eser kucuker <eserkucuker@gmail.com>
2022-02-07 15:10:44 +0300

    Add TaskDetailInteractor Class

--- ToDoApp/ToDoApp/Screens/TaskDetail/TaskDetailInteractor.swift
+++ ToDoApp/ToDoApp/Screens/TaskDetail/TaskDetailInteractor.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-class TaskDetailInteractor{
+class TaskDetailInteractor: TaskDetailInteractorProtocol, TaskDetailDataStoreProtocol {
+    
+    
+    var task: Tasks?
+    var presenter: TaskDetailPresenterProtocol?
+    var dataManager : DataManagerProtocol
+    
+    init(dataManager: DataManagerProtocol){
+        self.dataManager = dataManager
+    }
+
+    func viewdidload() {
+        guard let tasks = task else { return }
+        self.presenter?.handleOutput(.showTask(tasks))
+    }
+    
+    func addTask(task:TaskDetailPresentation) {
+        dataManager.saveData(task: task)
+    }
+    
+    func update(tasks: TaskDetailPresentation){
+        guard let todo = task else { return }
+        dataManager.updateData(todoItem: todo, title: tasks.title, subtitle: tasks.detail, date: tasks.deadlineDate)
+    }
 }
